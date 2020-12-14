d3ca48c9e2622775dcd58ebc8be77d43664b4a11
Jack <jack@mrph.dev>
2020-12-14 15:42:53 +0000

    first

---
+++ main.py
@@ -0,0 +1,17 @@
+from scholarly import scholarly
+import jsonpickle
+import json
+from datetime import datetime
+
+scholar_ids = ['RwMPs-8AAAAJ&hl']
+
+for id in scholar_ids:
+    author = scholarly.search_author_id(id)
+    scholarly.fill(author, sections=['basics', 'indices', 'publications'])
+    name = author['name']
+    author['updated'] = str(datetime.now)
+    scholarly.pprint(author)
+    author = jsonpickle.encode(author)
+
+    with open(f'{name}.json', 'w') as outfile:
+        json.dump(author, outfile)
