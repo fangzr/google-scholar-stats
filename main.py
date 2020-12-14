65481543cef961205428191c9e16f91a636d544d
Jack F. Murphy <jfm@jack.engineering>
2020-12-14 16:04:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ scholar_ids = ['RwMPs-8AAAAJ&hl']
 
 for id in scholar_ids:
     author = scholarly.search_author_id(id)
-    scholarly.fill(author, sections=['basics', 'indices', 'publications'])
+    scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
     name = author['name']
     author['updated'] = str(datetime.now)
     scholarly.pprint(author)
