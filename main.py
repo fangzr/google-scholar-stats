15e38e096bda3c9c398815c578f084b7bff5f6d1
Zhengru Fang <zhefang4-c@my.cityu.edu.hk>
2025-03-31 01:35:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import jsonpickle
 import json
 from datetime import datetime
 
-scholar_ids = ['RwMPs-8AAAAJ&hl']
+
+scholar_ids = ['yggQMJMAAAAJ']
 
 for id in scholar_ids:
     author = scholarly.search_author_id(id)
     scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
     name = author['name']
-    author['updated'] = str(datetime.now)
+    author['updated'] = str(datetime.now())  # 修复了datetime.now的调用
     scholarly.pprint(author)
     author = jsonpickle.encode(author)
 
